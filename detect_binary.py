0b0506240035dca48bf0ea5d43fb913a6f6af1ee
Quan Yuan <qyuan@aibee.com>
2018-09-21 17:08:34 -0700

    updates

--- detect_binary.py
+++ detect_binary.py
@@ -6,18 +6,21 @@ Quan Yuan
 
 import argparse
 import os
-from detector.misc import utils
+import detector.maskrcnn_detector as maskrcnn
 from data_utils import data_binary
 import cv2
 import numpy
+import collections
+import detector.misc.dependency as dependency
 
 
-def get_key_points_in_split(model, split_data, data_folder):
+def get_key_points_in_split(model, split_data, data_folder, keys, batch_size=64):
     keypoints = {}
-    for i, video_track in enumerate(split_data.keys()):
-        line = split_data[video_track]
+    for i, key in enumerate(keys):
+        line = split_data[key]
         images = []
-        for p in line:
+        keypoints_row = []
+        for i, p in enumerate(line):
             file_name, offset = p
             data_file = os.path.join(data_folder,file_name)
             if not os.path.isfile(data_file):
@@ -25,12 +28,40 @@ def get_key_points_in_split(model, split_data, data_folder):
                 continue
             image = data_binary.read_one_image(data_file, offset)
             images.append(image)
-        keypoints_batch = model.compute_features_on_batch(numpy.array(images))
-        keypoints[video_track] = keypoints_batch
+            if len(images) >= batch_size or i==len(line)-1:
+                keypoints_batch = model.compute_features_on_batch(numpy.array(images))
+                keypoints_row += keypoints_batch
+                images  = []
 
-        if (i+1) % 100 == 0:
+        #keypoints_batch = model.compute_features_on_batch(numpy.array(images))
+        keypoints[key] = keypoints_row
+        if (i+1) % 20 == 0:
             print("finished computing descriptor of pid/track_id {} out of {}".format(str(i+1), str(len(split_data))))
     return keypoints
+
+def load_list_to_pid(list_file, data_folder, path_tail_len=2):
+    pid_index = collections.defaultdict(list)
+    with open(list_file) as f:
+        lines = f.readlines()
+        for line in lines:
+            label = int(line.split()[0])
+            groups = line.strip().split()[1:]
+            num_imgs = int(len(groups) / 2)
+            # if label in pid_index:
+            #     print "pid label {} already in pid_index. might have a conflict!".format(str(label))
+            for i in range(num_imgs):
+                part_name = groups[2 * i]
+                within_idx = int(groups[2 * i + 1])
+                if len(data_folder) > 0:
+                    path_parts = os.path.normcase(part_name).split('/')[-path_tail_len:]
+                    path_tail = os.path.join(*path_parts)
+                    data_file = os.path.join(data_folder, path_tail)
+                else:
+                    data_file = part_name
+                if os.path.isfile(data_file):
+                    pid_index[label].append((data_file, within_idx))
+    return pid_index
+
 
 if __name__ == "__main__":
     # construct the argument parse and parse the arguments
@@ -39,9 +70,19 @@ if __name__ == "__main__":
     ap.add_argument("data_folder", type=str, help="path to data_folder")
     ap.add_argument("model_path", type=str, help="path to model file")
     ap.add_argument("output_folder", type=str, help="path to output file")
-    ap.add_argument("--gpu_id", type=int, help="gpu_device", default=7)
+    ap.add_argument("--gpu_count", type=int, help="count of gpu", default=1)
+    ap.add_argument("--batch_size", type=int, help="batch_size in detection", default=64)
     ap.add_argument("--start_line", type=int, help="line to start", default=0)
     ap.add_argument("--last_line", type=int, help="last line to process", default=300000)
     ap.add_argument("--sample_size", type=int, help="num per track", default=8)
     args = ap.parse_args()
+    pid_data = load_list_to_pid(args.index_file, args.data_folder)
+    pids = pid_data.keys()
 
+    inference_config = dependency.InferenceConfig()
+    inference_config.GPU_COUNT = args.gpu_count
+    inference_config.BATCH_SIZE = args.batch_size
+    print("detect keypoints with {} gpus and batch size of {}".format(str(args.gpu_count), str(args.batch_size)))
+    model = maskrcnn.load_model(args.model_file, args.output_folder, inference_config)
+
+    keypoints = get_key_points_in_split(model, pid_data, args.data_folder, pids, batch_size=args.batch_size)
--- detect_keypoints_in_image.py
+++ detect_keypoints_in_image.py
@@ -21,20 +21,22 @@ if __name__ == "__main__":
     ap.add_argument("--gpu_count", type=int, default=0, help="how many gpu to use")
     args = ap.parse_args()
 
+    batch_size = 4
 
     inference_config = dependency.InferenceConfig()
     inference_config.GPU_COUNT = args.gpu_count
+    inference_config.BATCH_SIZE = batch_size
     model = maskrcnn.load_model(args.model_file, args.output_folder, inference_config)
-
+    print("model path = {}".format(args.model_file))
     # if args.gpu_count > 0:
     #     model = parallel_model.ParallelModel(model, gpu_count=args.gpu_count)
     image_rgb = utils.read_one_image(args.image_file)
-    results = maskrcnn.detect_keypoints_one_image(model, image_rgb)
+    results = maskrcnn.detect_keypoints_images(model, [image_rgb]*batch_size)
     n = results.shape[0]
     image_result = cv2.cvtColor(image_rgb, cv2.COLOR_RGB2BGR)
     for k in range(n):
-        result = numpy.squeeze(results[k, :, :])
-        image_result = utils.plot_key_points(image_result, results[k, :, 0], results[k, :, 1])
+        result = results[k, :]
+        image_result = utils.plot_key_points(image_result, result[0, :, 0], results[0, :, 1])
     cv2.imshow('w', image_result)
     cv2.waitKey()
     pass
