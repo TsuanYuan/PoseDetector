96d00f74c02dc6ca7c95aea56a6886eab5a734a0
Quan Yuan <yqsony@gmail.com>
2018-09-23 14:37:04 -0700

    binary detections

--- detect_binary.py
+++ detect_binary.py
@@ -14,13 +14,40 @@ import collections
 import detector.misc.dependency as dependency
 import pickle
 
+def collage_images(images, w_count, h_count, norm_shape=(128,256)):
+    assert len(images) <= w_count*h_count
+    canvas = numpy.zeros((norm_shape[1]*h_count, norm_shape[0]*w_count, 3), dtype=numpy.uint8)
+    for i, image in enumerate(images):
+        im = cv2.resize(image, norm_shape)
+        row_id = int(i/w_count)
+        col_id = i%w_count
+        canvas[row_id*norm_shape[1]:(row_id+1)*norm_shape[1],col_id*norm_shape[0]:(col_id+1)*norm_shape[0], :] = im
+    return canvas
 
-def get_key_points_in_split(model, split_data, data_folder, keys, batch_size=64):
+
+def decode_keypoints_on_collages(keypoints_on_one_collage, w_count, h_count, norm_shape=(128, 256)):
+    keypoints_on_one_collage = numpy.squeeze(keypoints_on_one_collage)
+    mean_xy = numpy.squeeze(numpy.mean(keypoints_on_one_collage, axis=1))[:, :2].astype(int)
+    crop_idx = mean_xy[:, 1]/norm_shape[1]*w_count + mean_xy[:, 0]/norm_shape[0]
+    keypoints = collections.defaultdict(list)
+    n = keypoints_on_one_collage.shape[0]
+    for i in range(n):
+        col = crop_idx[i]%w_count
+        row = crop_idx[i]/w_count
+        kp = numpy.squeeze(keypoints_on_one_collage[i, :])-numpy.array([norm_shape[0]*col, norm_shape[1]*row,0,0])
+        keypoints[crop_idx[i]].append(kp / [norm_shape[0], norm_shape[1],1,1])
+    return keypoints
+
+
+def get_key_points_in_split(model, split_data, data_folder, keys, batch_size=8, w_count=8, h_count=4, norm_shape=(128, 256)):
     keypoints = {}
-    for i, key in enumerate(keys):
+    for m, key in enumerate(keys):
         line = split_data[key]
         images = []
-        keypoints_row = []
+        keypoints_page = []
+        collages = []
+        count = 0
+        keypoints_decode = collections.defaultdict(list)
         for i, p in enumerate(line):
             file_name, offset = p
             data_file = os.path.join(data_folder,file_name)
@@ -28,16 +55,30 @@ def get_key_points_in_split(model, split_data, data_folder, keys, batch_size=64)
                 print('fail to load data file {}'.format(data_file))
                 continue
             image = data_binary.read_one_image(data_file, offset)
+            image = cv2.resize(image, norm_shape)
             images.append(image)
-            if len(images) >= batch_size or i==len(line)-1:
-                keypoints_batch = maskrcnn.detect_keypoints_images(model, numpy.array(images))
-                keypoints_row += keypoints_batch
-                images  = []
+            if len(images) == w_count*h_count or i==len(line)-1:
+                collage = collage_images(images, w_count, h_count)
+                collages.append(collage)
+            # put w_count*h_count crops in one collage and detect keypoints. decode the keypoints to the xy in each crop afterwards
+            if len(collages) >= batch_size or i==len(line)-1:
+                keypoints_on_collages = maskrcnn.detect_keypoints_images(model, numpy.array(collages))
+                batch_len = len(collages)
 
+                for k in range(batch_len):
+                    keypoints_on_one_page = keypoints_on_collages[k]
+                    keypoints_row = decode_keypoints_on_collages(keypoints_on_one_page, w_count, h_count, norm_shape)
+                    # j is the id of a patch in current one collage
+                    for j in keypoints_row:
+                        # global id = k*w_count*h_count+j
+                        keypoints_decode[k*w_count*h_count+j]=keypoints_row[j]
+                count += w_count * h_count * batch_size
+                collages = []
+                images = []
         #keypoints_batch = model.compute_features_on_batch(numpy.array(images))
-        keypoints[key] = keypoints_row
-        if (i+1) % 20 == 0:
-            print("finished computing descriptor of pid/track_id {} out of {}".format(str(i+1), str(len(split_data))))
+        keypoints[key] = keypoints_page
+        if (m+1) % 20 == 0:
+            print("finished computing descriptor of pid/track_id {} out of {}".format(str(m+1), str(len(split_data))))
     return keypoints
 
 def load_list_to_pid(list_file, data_folder, path_tail_len=2):
--- detect_keypoints_in_image.py
+++ detect_keypoints_in_image.py
@@ -11,6 +11,7 @@ import detector.misc.dependency as dependency
 import detector.models.parallel_model as parallel_model
 import cv2
 import numpy
+import detect_binary
 
 if __name__ == "__main__":
     # construct the argument parse and parse the arguments
@@ -21,22 +22,34 @@ if __name__ == "__main__":
     ap.add_argument("--gpu_count", type=int, default=0, help="how many gpu to use")
     args = ap.parse_args()
 
-    batch_size = 4
+    batch_size = 1
 
     inference_config = dependency.InferenceConfig()
     inference_config.GPU_COUNT = args.gpu_count
     inference_config.BATCH_SIZE = batch_size
+
     model = maskrcnn.load_model(args.model_file, args.output_folder, inference_config)
     print("model path = {}".format(args.model_file))
     # if args.gpu_count > 0:
     #     model = parallel_model.ParallelModel(model, gpu_count=args.gpu_count)
     image_rgb = utils.read_one_image(args.image_file)
-    results = maskrcnn.detect_keypoints_images(model, [image_rgb]*batch_size)
-    n = results.shape[0]
-    image_result = cv2.cvtColor(image_rgb, cv2.COLOR_RGB2BGR)
-    for k in range(n):
+    image_rgb2 = utils.read_one_image('/Users/quan/Documents/data/wan/00059856/ch11010_20180821203030_00004021_00012850.jpg')
+    images = [image_rgb, image_rgb2, image_rgb, image_rgb2, image_rgb2, image_rgb]
+    images = [cv2.resize(im, (128, 256)) for im in images]
+    collage = detect_binary.collage_images(images, w_count=3, h_count=2)
+
+    results = maskrcnn.detect_keypoints_images(model, [collage])
+    results_decoded = detect_binary.decode_keypoints_on_collages(results, 3, 2)
+    s = results.shape #numpy.squeeze(results).shape
+    image_result = cv2.cvtColor(collage, cv2.COLOR_RGB2BGR)
+    for k in range(s[0]):
         result = results[k, :]
-        image_result = utils.plot_key_points(image_result, result[0, :, 0], result[0, :, 1])
-    cv2.imshow('w', image_result)
-    cv2.waitKey()
+        for j in range(s[1]):
+            image_result0 = utils.plot_key_points(images[j],
+                                                  results_decoded[j][0][:, 0]*images[j].shape[1],
+                                                  results_decoded[j][0][:, 1]*images[j].shape[0])
+            image_result2 = utils.plot_key_points(image_result, result[j, :, 0], result[j, :, 1])
+            cv2.imshow('h', image_result0)
+            #cv2.imshow('w', image_result2)
+            cv2.waitKey()
     pass
